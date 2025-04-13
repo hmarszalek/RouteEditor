7b650d53ba933577fe981cce68e3b31e93f2db5c
hmarszalek <marszalekhania5@gmail.com>
2025-04-13 21:39:33 +0200

    Create Route

--- routes/urls.py
+++ routes/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
-from .views import home, register_view, login_view, logout_view
+from .views import home, register_view, login_view, logout_view, create_route
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", home, name="home"),
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
+    path('create_route/', create_route, name='create_route'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- routes/views.py
+++ routes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 from .models import BackgroundImage, Route, RoutePoint
 
 def home(request):
@@ -32,3 +33,13 @@ def logout_view(request):
     logout(request)
     return redirect("home")
 
+@login_required
+def create_route(request):
+    if request.method == 'POST':
+        background_id = request.POST.get("background")
+        background = BackgroundImage.objects.get(id=background_id)
+        route_name = request.POST.get("name")
+        route = Route.objects.create(user=request.user, name=route_name, background=background)
+        return redirect("home")
+    backgrounds = BackgroundImage.objects.all()
+    return render(request, "routes/create_route.html", {"backgrounds": backgrounds})
