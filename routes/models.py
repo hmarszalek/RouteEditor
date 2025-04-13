ccdce60f9c5e9c0a2598f706936a6bd06126be94
hmarszalek <marszalekhania5@gmail.com>
2025-04-13 21:04:33 +0200

    Initial commit for RouteEditor

---
+++ routes/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class BackgroundImage(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/backgrounds/')
+
+    def __str__(self):
+        return self.name
+
+class Route(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    background = models.ForeignKey(BackgroundImage, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Route-of-{self.user}-{self.name}"
+
+class RoutePoint(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, related_name='points', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    x = models.PositiveIntegerField()
+    y = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"Point-of-{self.user}-on-Route-{self.route.name}-({self.x},{self.y})"
---
+++ routes/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import BackgroundImage, Route, RoutePoint
+
+def home(request):
+    return render(request, "routes/home.html")
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "routes/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "routes/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
+
